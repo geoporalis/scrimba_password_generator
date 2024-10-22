8f381ed7659ef439b61a2d3da4a8038c5bb4f048
geoporalis <georg.poller@gmail.com>
2024-10-22 14:00:20 +0200

    init

---
+++ index.js
@@ -0,0 +1,98 @@
+// const chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+// "/"];
+// console.log(chars.length, chars)
+
+const bigletters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const smallletters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specials= ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let generateBtn = document.getElementById("generate-btn");
+let passoneBtn = document.getElementById("passone-btn");
+let passtwoBtn = document.getElementById("passtwo-btn");
+let lengthIpt = document.getElementById("length-ipt");
+let smallChk = document.getElementById("small-ipt");
+let bigChk = document.getElementById("big-ipt");
+let numChk = document.getElementById("num-ipt");
+let charChk = document.getElementById("char-ipt");
+let passone;
+let passtwo;
+
+generateBtn.addEventListener("click", generateRandomPassword);
+passoneBtn.addEventListener("click",
+// () => {console.log(passone);}
+    navigator.clipboard.writeText(passone).then(
+        function() {
+            console.log('Async: Copying to clipboard was successful!');
+        }, 
+        function(err) {
+            // console.error('Async: Could not copy text: ');//, err);
+        }
+    )
+);
+
+passtwoBtn.addEventListener("click", 
+// () => {console.log(passtwo);}
+    navigator.clipboard.writeText(passtwo).then(
+        function() {
+            console.log('Async: Copying to clipboard was successful!');
+        }, 
+        function(err) {
+            // console.error('Async: Could not copy text: ');//, err);
+        }
+    )
+);
+
+const passwordLength = 15;
+
+function getRandomCharacter(characters) {
+    
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generateRandomPassword() {
+    const characters = getCharacterArray();
+    const passwordLength = (lengthIpt.value !== "") ? lengthIpt.value : lengthIpt.placeholder;
+        
+    if(characters.length < 9){
+        passone = 'please review';
+        passtwo = 'your selection';
+    } else {
+        let randomPassword = ""
+        for (let i = 0; i < passwordLength*2; i++) {
+            randomPassword += getRandomCharacter(characters);         
+        }
+        passone = randomPassword.slice(0,passwordLength);
+        passtwo = randomPassword.slice(passwordLength);
+        
+    }
+    
+    
+    passoneBtn.innerHTML = esc(passone);
+    passtwoBtn.innerHTML = esc(passtwo);
+    
+    console.log(passone,passtwo);
+}
+
+function getCharacterArray() {
+    let characters = [];
+    
+    if (smallChk.checked) {
+        characters.push(...smallletters)
+    }
+    if (bigChk.checked) {
+        characters.push(...bigletters)
+    }
+    if (numChk.checked) {
+        characters.push(...numbers)
+    }
+    if (charChk.checked) {
+        characters.push(...specials)
+    }
+    
+    return characters;
+}
+
+function esc(str){ return new Option(str).innerHTML; }
